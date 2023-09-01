831f7c0a63596a954a2f24edbda2d002c687dce6
harelpanker <harelpanker@gmail.com>
2023-09-01 15:51:04 +0200

    button style

--- app/[book]/[chapter]/ChapterContent.tsx
+++ app/[book]/[chapter]/ChapterContent.tsx
@@ -36,17 +36,18 @@ const ChapterContent: FC<ChapterContentProps> = ({
       {chapter && chepterNumber && (
         <div className='flex'>
           <DropdownMenu>
-            <DropdownMenuTrigger className='rounded py-1 px-3 border border-slate-200 bg-slate-100 flex items-center justify-center gap-2'>
+            <DropdownMenuTrigger className='flex items-center justify-center gap-2 rounded border border-slate-200 bg-slate-100 px-3 py-1'>
               <span>Chapter {chapter}</span>{' '}
-              <ChevronDown className='text-slate-700 w-4 h-4' />
+              <ChevronDown className='h-4 w-4 text-slate-700' />
             </DropdownMenuTrigger>
-            <DropdownMenuContent className='flex flex-col gap-2 max-h-72 overflow-auto'>
+            <DropdownMenuContent className='flex max-h-72 flex-col gap-2 overflow-auto'>
               {Array.from(
                 { length: chepterNumber?.chapterCount },
                 (_, i) => i + 1
               ).map((id) => (
-                <DropdownMenuItem key={id}>
+                <DropdownMenuItem key={id} className='p-0'>
                   <Link
+                    className='w-full px-2 py-[6px]'
                     href={`/${chapterD[0].b}/${id}`}>{`Chapter ${id}`}</Link>
                 </DropdownMenuItem>
               ))}
@@ -55,13 +56,13 @@ const ChapterContent: FC<ChapterContentProps> = ({
         </div>
       )}
 
-      <ul className='grid grid-cols-1 gap-6 divide-y divide-y-slate-300'>
+      <ul className='divide-y-slate-300 grid grid-cols-1 gap-6 divide-y'>
         {chapterD.map((item) => (
-          <li className='pt-6 first:pt-0 flex flex-col gap-5' key={item.id}>
+          <li className='flex flex-col gap-5 pt-6 first:pt-0' key={item.id}>
             <div className={`flex w-full justify-between`}>
               {!share ? (
                 <Link href={`/${item.b}/${item.c}/${item.id}`}>
-                  <LinkIcon className='w-4 h-4' />
+                  <LinkIcon className='h-4 w-4' />
                 </Link>
               ) : (
                 <Share />
@@ -70,7 +71,7 @@ const ChapterContent: FC<ChapterContentProps> = ({
                 {item.c} : {item.v}
               </div>
             </div>
-            <p className='text-xl leading-relaxed lg:text-3xl lg:leading-relaxed font-medium max-w-4xl'>
+            <p className='max-w-4xl text-xl font-medium leading-relaxed lg:text-3xl lg:leading-relaxed'>
               {item.t}
             </p>
           </li>
